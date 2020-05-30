7c6d38a59e2bac51af7300bace6adf1e4efcc923
Radosław Rocławski <r.roclawski@ankara.pl>
2020-05-30 09:56:42 +0200

    corrected tetromino grid and placed first object

--- app.js
+++ app.js
@@ -44,4 +44,16 @@ const iTetromino = [
 
 const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
 
+let currentPosition = 4;
+let current = theTetrominoes[0][0];
+
+//draw the first rotation in the first tetromino
+function draw() {
+    current.forEach(index => {
+        squares[currentPosition + index].classList.add('tetromino');
+    })
+}
+
+draw();
+
 });
